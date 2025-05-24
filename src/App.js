fe84d0ffc93d7827e706b60fc06e430db3284144
Ju-bari <wngud1225@gmail.com>
2025-05-24 17:16:22 +0900

    [feat] Component 생성, props, event 연습 코드 제작 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header(props) {
+  console.log('props', props);
+  console.log('>>>', props.title);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <header>
+      <h1><a href='/' onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  )
+}
+
+function Nav(props) {
+  const list = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let topic = props.topics[i];
+    list.push(
+      <li key={topic.id} title={topic.body}>
+        <a href={'/read/' + topic.id} onClick={(event)=>{
+          event.preventDefault();
+          props.onChangeMode(topic.id);
+        }}>
+        {topic.title}
         </a>
-      </header>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>
+        {list}
+      </ol>
+    </nav>
+  )
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+    </article>
+  )
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: 'HTML', body: 'HTML is HyperText Markup Language.' },
+    { id: 2, title: 'CSS', body: 'CSS is Cascading Style Sheets.' },
+    { id: 3, title: 'JavaScript', body: 'JavaScript is a programming language.' }
+  ]
+  return (
+    <div>
+      <Header title="REACT" onChangeMode={()=>{
+        alert('header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+
+      <Article title="Welcome" body="Hello, WEB!"></Article>
+      <Article title="Welcome" body="Hello, React!"></Article>
+      <Article title="Welcome" body="Hello, Spring!"></Article>
     </div>
   );
 }
